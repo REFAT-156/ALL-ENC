adc08ad303ca9b77983b9b2464e62f9d2502f7b0
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-01-06 13:03:57 +0600

    Update Enc.py

--- Enc.py
+++ Enc.py
@@ -1,18 +1,23 @@
 import os, platform, time
-print('\n\x1b[1;37m[●] Checking Update.....');time.sleep(0.5)
+def xoss(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.02)
+xoss('\n\x1b[1;37m[●] Checking Update.....');time.sleep(0.5)
 def Update():
     exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
 def Run():
         bit = platform.architecture()[0]
         if bit == '64bit':
-            print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
-            print('[●] Follow My Github First')
+            xoss("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            xoss('[●] Follow My Github First')
             os.system('xdg-open https://github.com/REFAT-156')
             from allenc import REFATMENU
             REFATMENU()
         elif bit == '32bit':
-            print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
-            print('[●] Follow My Github First')
+            xoss("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            xoss('[●] Follow My Github First')
             os.system('xdg-open https://github.com/REFAT-156')
             from allenc import REFATMENU
             REFATMENU()
