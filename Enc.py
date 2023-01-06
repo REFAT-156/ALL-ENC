a8bb770b26188462736d54b294b41e96e9b9d292
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2023-01-06 12:58:47 +0600

    Create Enc.py

---
+++ Enc.py
@@ -0,0 +1,21 @@
+import os, platform, time
+print('\n\x1b[1;37m[●] Checking Update.....');time.sleep(0.5)
+def Update():
+    exit('\033[1;31m[●] Commands On Update Please Wait For Update ❤ ')
+def Run():
+        bit = platform.architecture()[0]
+        if bit == '64bit':
+            print("\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from rfile import REFATMENU
+            REFATMENU()
+        elif bit == '32bit':
+            print("\n\x1b[1;92m[●] Congratulations ! Your Device Support this Tools")
+            print('[●] Follow My Github First')
+            os.system('xdg-open https://github.com/REFAT-156')
+            from ffile import REFATMENU
+            REFATMENU()
+        else:
+            exit('\033[1;31m[●] Connection & Network Error')
+Run()
